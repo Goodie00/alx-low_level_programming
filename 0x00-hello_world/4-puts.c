14474dc5b07db5aa2fe2c6f90241bd91c9b6e0f2
Goodie00 <goodieomolayo1@gmail.com>
2022-09-08 12:51:47 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,7 @@ include<stdio.h>
  * Return: 0
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	Return (0)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
 }
