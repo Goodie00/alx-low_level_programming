c2467e1b92d521dd207e9bad30ec97739c1b5ba4
Goodie00 <goodieomolayo1@gmail.com>
2022-09-09 20:39:43 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Description: using the main function
+ * this program prints "Programming is positive, zero or negative"
+ * Return: 0
+ */
+int main (void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+	}
+	putchar(c);
+}
+}
+putchar('\n');
+return (0);
+}
+
