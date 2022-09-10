6e33d3ca7cecf1361b30eca9797b8198cbbd287f
Goodie00 <goodieomolayo1@gmail.com>
2022-09-09 21:49:35 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print if number is positive, zero, or negative
+ * Return 0
+ */
+int main (void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c++)
+	{
+		putchar(c);
+	}putchar('\n');
+	return (0);
+}
