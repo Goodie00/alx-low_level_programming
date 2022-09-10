dc56e2eae2504d011e9093f69b2f56b7467cd3e8
Goodie00 <goodieomolayo1@gmail.com>
2022-09-09 20:27:50 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
  * main - print if the number is positive, zero or negative
  *
  * Description: using the main function
- * this program prints "Programming is positive, zero or negative
+ * this program prints "Programming is positive, zero or negative"
  * Return: 0
  */
 int main(void)
@@ -12,11 +12,11 @@ int main(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(c)
+		putchar(c);
 	}
 	for (c = 'A'; c <= 'Z'; c++)
 	{
-		putchar(c)
+		putchar(c);
 	}
 	putchar('\n');
 
