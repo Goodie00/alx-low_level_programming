174b3f9aaeae552030a2529da7140fc880bb6860
Goodie00 <goodieomolayo1@gmail.com>
2022-09-11 15:58:28 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,12 +3,12 @@
  * main - main block
  * Description: prints all single digit numbers of base 10
  * starting from 0, followed by a new line
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
 	char c = 'z';
-	while c >= 'a';
+	while (c >= 'a')
 	{
 		putchar(c);
 		c--;
