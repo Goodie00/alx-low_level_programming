24254fce5f78987aa06bcfb00243065552325730
Goodie00 <goodieomolayo1@gmail.com>
2022-09-13 12:59:10 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,18 +8,12 @@
  */
 int _abs(int c)
 {
-	if (c > 0) 
-	{
-		return (c * (-1));
-	}
-
-	else if (c == 0)
-	{
-		return (0);
-	}
-
-	else
+	if (c > 0 || c == 0) 
 	{
 		return (c);
+	}
+	else
+	{
+		return (c * -1);
 	}
 }
