0fd3dc8f00cf5635659950b62a6b551bafdfd6d6
Goodie00 <goodieomolayo1@gmail.com>
2022-09-13 17:17:12 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,8 +24,14 @@ void print_times_table(int n)
 			_putchar(' ');
 			_putchar('0' + rep);
 		}
-		else
+		else if (rep < 100)
 		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+	}
 			_putchar('0' + rep / 100);
 			_putchar('0' + (rep - 100) / 10);
 			_putchar('0' + rep % 10);
