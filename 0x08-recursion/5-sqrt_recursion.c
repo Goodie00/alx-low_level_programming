f4bcbe99ebfe5aaf48ffc366b0016c0e34892e4e
Goodie00 <goodieomolayo1@gmail.com>
2022-09-28 07:08:30 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,10 +9,9 @@ int _sqrt_recursion(int n)
 {
 	int i = 0;
 
-	if (i < 0) /*If n is negative*/
+	if (i < 0)
 		return (-1);
-	else
-	{
-		return (_evaluate(i, n)); /*Recursive call*/
-	}
+	else if (i == 0)
+		return (1);
+	return (_evaluate(i, n));
 }
