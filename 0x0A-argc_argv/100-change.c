951453430b821f9c81e4fdac3e2ad12f673549d0
Goodie00 <goodieomolayo1@gmail.com>
2022-09-30 09:07:56 -0700

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,6 +18,14 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
+		return (1);
+	}
+
+	total = atoi(argv[1]); /*Covert str to int*/
+
+	if (total <= 0)
+	{
+		printf("0\n");
 		return (0);
 	}
 
