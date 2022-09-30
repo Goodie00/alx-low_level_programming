8a5ab0a2c74fdc8a69a8282e763f9d01d474b5c2
Goodie00 <goodieomolayo1@gmail.com>
2022-09-30 11:31:28 -0700

    Task 0

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-<<<<<<< HEAD
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-ar -rc liball.a *.o
-=======
-gcc -c *.c
-ar rc liball.a *.o
-ranlib liball.a
->>>>>>> b31fb86cdcddb5494f5516d047fd06f0a8883a50
