a6e378d22a920f0ea9ba9d7fbad6deb7ccdb1fce
Goodie00 <goodieomolayo1@gmail.com>
2022-10-20 14:22:00 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,15 +16,14 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 
-	 new->str = strdup(str);
+	new->str = strdup(str)
+		;
 
-	 for (nchar = 0; str[nchar]; nchar++)
-		 ;
+	for (nchar = 0; str[nchar]; nchar++);
 
-	  new->len = nchar;
-	  new->next = *head;
-	  *head = new;
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
 
-	  return (*head);
+	return (*head);
 }
-
